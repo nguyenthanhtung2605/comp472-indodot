a245e9affcb7b8c1d233351bd9df2b3b336ba2ae
Thanh Nguyen <thanh.nguyen05@sap.com>
2020-01-30 10:16:00 -0500

    tung

--- player.py
+++ player.py
@@ -2,8 +2,10 @@
 Player library containing basic move operations.
 """
 
+
 def move_left(first_term, second_term):
     return first_term + second_term
+
 
 def move_right(first_term, second_term):
     return first_term - second_term
