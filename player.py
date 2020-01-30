dbf4e3d7a9ffc254349af98a9bb7720cdf5b9ac7
Thanh Nguyen <thanh.nguyen05@sap.com>
2020-01-29 19:27:45 -0500

    Add functions for basic moves

---
+++ player.py
@@ -0,0 +1,9 @@
+"""
+Player library containing basic move operations.
+"""
+
+def move_left(first_term, second_term):
+    return first_term + second_term
+
+def move_right(first_term, second_term):
+    return first_term - second_term
